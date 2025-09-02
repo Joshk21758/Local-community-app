629b3488b2568dfce0842e28d0d140711018c452
Mwansa Kunda <mwansakunda530@gmail.com>
2025-09-02 12:02:48 +0000

    can you remove the settings link from the dropdown menu

--- src/components/user-nav.jsx
+++ src/components/user-nav.jsx
@@ -38,7 +38,6 @@ export function UserNav() {
           <DropdownMenuItem asChild>
             <Link href="/profile">Profile</Link>
           </DropdownMenuItem>
-          <DropdownMenuItem>Settings</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
